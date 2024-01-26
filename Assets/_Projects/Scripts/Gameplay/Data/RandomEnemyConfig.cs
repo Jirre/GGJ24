26f768c7ac58c7f22c60c10e1e99ad7bd4bdbe30
Jirre Verkerk <jirreamon@gmail.com>
2024-01-26 20:28:33 +0100

    [+] Implemented Enemies

---
+++ Assets/_Projects/Scripts/Gameplay/Data/RandomEnemyConfig.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Project.Gameplay
+{
+    public class RandomEnemyConfig : AEnemyConfig
+    {
+        [SerializeField] private Vector2 _MinRange;
+        public Vector2 MinRange => _MinRange;
+        [SerializeField] private Vector2 _MaxRange;
+        public Vector2 MaxRange => _MaxRange;
+    }
+
+    public class RandomEnemyContext : AEnemyContext<RandomEnemyConfig>
+    {
+        public float Speed;
+    }
+}
