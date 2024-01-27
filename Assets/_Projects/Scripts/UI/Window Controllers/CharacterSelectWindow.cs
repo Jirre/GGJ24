fcfdc18b3f4efa0a4e727a73aa880807847d6cda
Jirre Verkerk <jirreamon@gmail.com>
2024-01-27 01:26:35 +0100

    [+] Player management

---
+++ Assets/_Projects/Scripts/UI/Window Controllers/CharacterSelectWindow.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using JvLib.Windows;
+using UnityEngine;
+
+namespace Project.UI.Windows
+{
+    public class CharacterSelectWindow : Window, IOnWindowShow
+    {
+        public void OnWindowShow(object context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
